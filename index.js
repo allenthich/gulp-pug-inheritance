f408635e8c67f1c4ecf6272e672b51ddb76b4e0d
pure180 <info@daniel-pfisterer.de>
2017-03-01 09:29:54 +0100

    check if file still exist, if not remove it
    Check if file still exist, if not remove it from temp object.

--- index.js
+++ index.js
@@ -82,19 +82,22 @@ var GulpPugInheritance = (function() {
     return path.replace( /\/|\\|\\\\|\-|\.|\:/g, '_' );
   };
 
-  GulpPugInheritance.prototype.getDependencies = function( file ) {
+  GulpPugInheritance.prototype.getDependencies = function( file, pathToFile ) {
     var _this         = this,
         dependencies  = [],
-        contents      = fs.readFileSync(file.path, 'utf8'),
-        dirname       = path.dirname(file.relative),
-        lex           = pugLex(contents, {
-                        filename: file.relative
-                      });
+        fileRelative  = ( typeof file === 'object' ) ? file.relative : file,
+        filePath      = ( typeof file === 'object' ) ? file.path : pathToFile,
+        contents      = ( fs.existsSync(filePath) ) ? fs.readFileSync( filePath, 'utf8' ) : false,
+        dirname       = path.dirname( fileRelative );
 
-    var parse = pugParser(lex);
-    var walk  = pugWalk(parse, function(node){
+    if ( contents === false ) {return;}
+    var lex           = pugLex(contents, {
+                        filename: fileRelative
+                      }),
+        parse = pugParser( lex );
+    var walk  = pugWalk( parse, function( node ){
       if ( node.type === 'Include' || node.type === 'Extends' ) {
-        var pathToDependencie =path.join(dirname, node.file.path );
+        var pathToDependencie =path.join( dirname, node.file.path );
         if ( _.indexOf( dependencies, pathToDependencie ) === -1 ) {
           dependencies.push( pathToDependencie );
         }
@@ -107,7 +110,19 @@ var GulpPugInheritance = (function() {
     var cacheKey = this.setTempKey( dependency );
     var pathToFile = path.join( process.cwd(), this.options.basedir, path.normalize( dependency ) );
     if ( this.tempInheritance[cacheKey] ) {
+      this.tempInheritance[cacheKey] = {};
       this.tempInheritance[cacheKey] = this.getInheritance( pathToFile );
+      this.tempInheritance[cacheKey].dependencies = this.getDependencies( dependency, pathToFile );
+      this.tempInheritance[cacheKey].file = dependency;
+    }
+  };
+
+  GulpPugInheritance.prototype.updateDependencies = function( dependencies ) {
+    var _this = this;
+    if ( dependencies.length > 0 ) {
+      _.forEach( dependencies, function( dependency ) {
+        _this.updateTempInheritance( dependency );
+      });
     }
   };
 
@@ -116,19 +131,14 @@ var GulpPugInheritance = (function() {
         cacheKey = this.setTempKey( file.relative ),
         inheritance =  this.getInheritance( file.path );
 
+    this.tempInheritance[cacheKey] = {};
     this.tempInheritance[cacheKey] = inheritance;
-    this.tempInheritance[cacheKey].dependencies = this.getDependencies(file);
+    this.tempInheritance[cacheKey].dependencies = this.getDependencies( file );
     this.tempInheritance[cacheKey].file = file.relative;
 
     if ( this.firstRun === false ) {
-      if (this.tempInheritance[cacheKey].dependencies.length > 0) {
-        _.forEach(this.tempInheritance[cacheKey].dependencies, function(dependency) {
-          _this.updateTempInheritance(dependency);
-        });
-      }
+      this.updateDependencies( this.tempInheritance[cacheKey].dependencies );
     }
-
-    fs.writeFileSync( this.tempFile, JSON.stringify(this.tempInheritance, null, 2), 'utf-8' );
 
     return inheritance;
   };
@@ -152,7 +162,7 @@ var GulpPugInheritance = (function() {
       }
     }
     var timeElapsed = (Date.now() - date);
-    console.log('[' + PLUGIN_NAME + '][' + state + '] Get inheritance of: "' + file.relative + '" - ' + timeElapsed + 'ms');
+    // console.log('[' + PLUGIN_NAME + '][' + state + '] Get inheritance of: "' + file.relative + '" - ' + timeElapsed + 'ms');
 
     return inheritance;
   };
@@ -164,9 +174,10 @@ var GulpPugInheritance = (function() {
   };
 
   GulpPugInheritance.prototype.endStream = function() {
+    var _this = this;
     if ( this.files.length ) {
-      var _this = this;
 
+      /*
       if ( this.options.saveInTempFile === true ) {
         if ( this.firstRun === true ) {
           console.log('[' + PLUGIN_NAME + '] Plugin started for the first time. Save inheritances to a tempfile');
@@ -174,6 +185,7 @@ var GulpPugInheritance = (function() {
           console.log('[' + PLUGIN_NAME + '] Plugin already started once. Get inheritances from a tempfile');
         }
       }
+      */
 
       _.forEach( this.files, function( file ) {
         var inheritance = _this.resolveInheritance( file );
@@ -199,9 +211,23 @@ var GulpPugInheritance = (function() {
       } else {
         this.stream.emit('end');
       }
-
     } else {
       this.stream.emit('end');
+    }
+
+    if ( this.options.saveInTempFile === true ) {
+      if ( _.size(this.tempInheritance) > 0 ) {
+        _.forEach( this.tempInheritance, function( tempInheritance ) {
+          var cacheKey = _this.setTempKey( tempInheritance.file );
+          var baseDir = path.join( process.cwd(), _this.options.basedir, tempInheritance.file );
+          if ( !fs.existsSync( baseDir ) ) {
+            _this.updateDependencies( tempInheritance.dependencies );
+            _this.tempInheritance[cacheKey] = undefined;
+          }
+        });
+      }
+
+      fs.writeFileSync( this.tempFile, JSON.stringify( this.tempInheritance, null, 2 ), 'utf-8' );
     }
   };
 
